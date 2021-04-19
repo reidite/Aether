8fc950c786619f5b04c2d1d7d35c5598afd40e0c
reidite <nglequocviet@gmail.com>
2021-04-19 17:36:22 +0700

    Prototype

--- app/src/main/java/com/example/aether/ApplicationContextSingleton.java
+++ app/src/main/java/com/example/aether/ApplicationContextSingleton.java
@@ -1,4 +1,20 @@
 package com.example.aether;
 
+import android.app.Activity;
+import android.content.Context;
+
 public class ApplicationContextSingleton {
+    private static Activity gContext;
+
+    public static void setContext( Activity activity) {
+        gContext = activity;
+    }
+
+    public static Activity getActivity() {
+        return gContext;
+    }
+
+    public static Context getContext() {
+        return gContext;
+    }
 }
